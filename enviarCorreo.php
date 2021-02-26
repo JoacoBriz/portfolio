df9f80fba1daf5b815fcd9262594f298a9e4bf03
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-26 18:17:25 -0300

    .

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -1,8 +1,8 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['asunto'];
-$empresa = $_POST['mensaje'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -15,7 +15,7 @@ $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'joaquinbriz1@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+$asunto = 'Mensaje de mi portfolio';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
