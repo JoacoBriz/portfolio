1f875566c75481e2278bbaa436da5d51e406b04d
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-26 19:32:33 -0300

    .

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -1,23 +1,15 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$asunto = $_POST['asunto'];
-$mensaje = $_POST['mensaje'];
+  $destinatario = "joaquinbriz1@gmail.com";
+  $nombre = $_POST["nombre"];
+  $email = $_POST["email"];
+  $asunto = $_POST["asunto"];
+  $mensaje = $_POST["mensaje"];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+  $header = 'From: ' . $mail . " \r\n";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+  mail($destinatario, $asunto, $mensaje, $header);
 
-$para = 'joaquinbriz1@gmail.com';
-$asunto = 'Mensaje de mi portfolio';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+
 ?>
